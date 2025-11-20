b7a6b0571625b7c20d95bc02ea57f3961fb9b9bc
Javad Mohammadi <javadmohammadi1994@gmail.com>
2025-11-20 20:48:28 +0330

    آماده‌سازی برای Render + gunicorn

--- app.py
+++ app.py
@@ -118,5 +118,5 @@ def logout():
     return redirect(url_for('login'))
 
 if __name__ == '__main__':
-    init_db()  # ساخت دیتابیس در اولین اجرا
-    app.run(debug=True)
+    init_db()
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)), debug=False)
